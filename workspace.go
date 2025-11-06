2fe8348134766e05c913e6d61b7306c5850b4294
sansmoraxz <souyamadebnath@gmail.com>
2025-11-07 01:15:36 +0530

    add server capability: workspace folders

---
+++ workspace.go
@@ -0,0 +1,27 @@
+package hyprls
+
+import (
+	"context"
+
+	"go.lsp.dev/protocol"
+)
+
+func extractIgnores(params *protocol.DidChangeConfigurationParams) (ignores []string) {
+	if settings, ok := (params.Settings).(map[string]any); ok {
+		if hyprls, ok := settings["hyprls"].(map[string]any); ok {
+			settings = hyprls
+		}
+
+		if ignore, ok := settings["ignore"]; ok {
+			if arr, ok := ignore.([]string); ok {
+				ignores = append(ignores, arr...)
+			}
+		}
+	}
+	return
+}
+
+func (h Handler) DidChangeConfiguration(ctx context.Context, params *protocol.DidChangeConfigurationParams) error {
+	ignores = extractIgnores(params)
+	return nil
+}
